698ac57d9f9219efe2f35d197ba481e0824b7fe9
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-23 03:18:19 -0400

    Set up ESLint and Prettier, and created corresponding ignore and config files

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Importing express module
+const express = require('express')
 
+// Create an instance of express
+const app = express()
+
+// Define a port to listen for incoming requests
+const PORT = process.env.PORT || 3000
+
+// Middleware to parse JSON and urlencoded data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// A simple route for the home page
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+  res.send('Hello, World!')
+})
 
+// Starting the server on the defined PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
